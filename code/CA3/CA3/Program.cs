e7b76625569b4ea0bd4dd62c4f05a910e58e7d7d
Cianan Nicolai <cianannicolai@gmail.com>
2022-12-14 14:57:01 +0000

    catagories list!

--- code/CA3/CA3/Program.cs
+++ code/CA3/CA3/Program.cs
@@ -1,3 +1,4 @@
+using BlazorStrap;
 using CA3;
 using CA3.Cocktails;
 using Microsoft.AspNetCore.Components.Web;
@@ -6,6 +7,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddBlazorStrap();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<ICocktailService, CocktailService>();
