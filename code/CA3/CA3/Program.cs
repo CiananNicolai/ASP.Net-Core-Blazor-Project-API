bfed93794eaa8087ee87a4c38faeed364fa3a2a5
Cianan Nicolai <cianannicolai@gmail.com>
2022-12-14 20:13:12 +0000

    mostly where it should be

--- code/CA3/CA3/Program.cs
+++ code/CA3/CA3/Program.cs
@@ -1,6 +1,5 @@
 using BlazorStrap;
 using CA3;
-using CA3.Cocktails;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -10,6 +9,5 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddBlazorStrap();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped<ICocktailService, CocktailService>();
 
 await builder.Build().RunAsync();
